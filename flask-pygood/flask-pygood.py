77cb452a1234386ab7409be348e2e40a0459a567
David Gomes <davidrafagomes@gmail.com>
2016-07-22 08:18:40 -0700

    Creates code structure in flask-pygood.py.

---
+++ flask-pygood/flask-pygood.py
@@ -0,0 +1,9 @@
+import good as G
+from functools import wraps
+
+class EndpointDecorator(object):
+    def __init__(self, schema):
+        pass
+
+    def __call__(self, func, *args, **kwargs):
+        pass
