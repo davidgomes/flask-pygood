9b4baa6c0c91d8c7bd74838811516c1568b31955
David Gomes <davidrafagomes@gmail.com>
2016-07-24 11:31:16 -0700

    Implements the decorator endpoint.

--- flask-pygood/flask-pygood.py
+++ flask-pygood/flask-pygood.py
@@ -1,9 +1,23 @@
 import good as G
 from functools import wraps
 
-class EndpointDecorator(object):
-    def __init__(self, schema):
+class EndpointDecorator:
+    def __init__(self, schema, callback, *args, **kwargs):
         pass
 
     def __call__(self, func, *args, **kwargs):
-        pass
+        def func_wrapper(func):
+            @wraps(func)
+            def returned_wrapper(*args, **kwargs):
+                request_data = {}
+
+                try:
+                    test_schema = schema(request_data)
+                except G.schema.errors.Invalid:
+                    callback()
+
+                return func(test_schema)
+
+            return returned_wrapper
+
+        return func_wrapper
