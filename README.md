dfd0a8abbf76679fecbb3fb103c3d43f1a8ffea7
David Gomes <davidrafagomes@gmail.com>
2016-07-31 19:32:42 -0700

    adds running instructions to readme.md.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # flask-pygood
 A wrapper that allows you to use Pygood easily with Flask endpoints
+
+## Setting up the source code
+```
+virtualenv-3.3 env
+source env/bin/activate
+pip install -r requirements.txt
+python3 -m flask_pygood.test.demo
+```
--- flask_pygood/flask_pygood.py
+++ flask_pygood/flask_pygood.py
@@ -1,5 +1,5 @@
 import good as G
-from flask import abort
+from flask import abort, request
 
 from functools import wraps
 
@@ -8,11 +8,18 @@ class EndpointDecorator:
         pass
 
     def __call__(self, func, *args, **kwargs):
+        print("heyy")
+        
         def func_wrapper(func):
             @wraps(func)
             def returned_wrapper(*args, **kwargs):
                 request_data = {}
+
+                print(dict(request.args))
                 
+                # if request.method == "GET":
+                    # request_dataz = {**request_data, **y}
+
                 try:
                     test_schema = schema(request_data)
                 except G.schema.errors.Invalid:
